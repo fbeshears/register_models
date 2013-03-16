9ce9fc18ea1e6f5e67ac97672a3b220cbb068547
fbeshears <fmb@berkeley.edu>
2013-03-15 22:23:51 -0700

    Create register_models.js

---
+++ register_models.js
@@ -0,0 +1,36 @@
+// register_models.js
+
+/*
+this will  initialize all of your Schemas and
+register all your models for a given node session.
+see the readme.txt file for a further discussion
+
+from another file (e.g. main.js), this can be called with:
+
+var register_models = require('./models/register_models');
+register_models();
+*/
+
+(function(){
+
+  module.exports = function(){
+
+		var mongoose = require('mongoose');
+		var Schema = mongoose.Schema; 
+
+		var files = ['kitten.js', 'comments.js'];
+
+		for(fn in files) {
+
+			var path_fn = "./" + files[fn];
+
+			var exported_model = require(path_fn);
+
+			exported_model(mongoose, Schema);
+
+		}
+
+
+	};
+
+})();
